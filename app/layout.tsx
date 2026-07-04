8ff2ad2fef023bbdd58edade26a06838bfdba7c8
kineab <kineabm21@gmail.com>
2026-07-04 21:44:45 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "@/app/globals.css";
+// app/layout.tsx
+import './globals.css'
 
 export const metadata: Metadata = {
   title: "Short Video Essay & Script Writer",
